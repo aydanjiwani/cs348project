4e095851f263bbdfcecf01f92805962fc7aec64e
aydanjiwani <a9jiwani@uwaterloo.ca>
2023-03-01 21:54:37 -0500

    first commit

---
+++ cs348main.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'world'
+mysql = MySQL(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/test')
+def testfn():
+    print(request.args.get('param1'))
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM city LIMIT 2")
+    data = cur.fetchall()
+    cur.close()
+    return str(data)
+
+
+@app.route('/createflight')
+def createflight():
+    route_id = request.args.get('route_id')
+    start = request.args.get('start')
+    end = request.args.get('end')
+    airline = request.args.get('airline')
+    print(route_id,start,end,airline)
+    with open('createflight.txt', 'r') as f:
+        query = f.read().replace('{route_id}', route_id)
+        query = f.read().replace('{start}', start)
+        query = f.read().replace('{end}', end)
+        query = f.read().replace('{airline}', airline)
+    cur = mysql.connection.cursor()
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
+    return str(data)
+
+
+@app.route('/findflight')
+def findflight():
+    src = request.args.get('src')
+    dest = request.args.get('dest')
+    with open('findflight.txt', 'r') as f:
+        query = f.read().replace('{src}', src)
+        query = f.read().replace('{dest}', dest)
+    cur = mysql.connection.cursor()
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
+    return str(data)
+
+@app.route('/buyticket')
+def buyticket():
+    f_id = request.args.get('f_id')
+    p_id = request.args.get('p_id')
+    with open('buyticket.txt', 'r') as f:
+        query = f.read().replace('{f_id}', f_id)
+        query = f.read().replace('{p_id}', p_id)
+    cur = mysql.connection.cursor()
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
+    return str(data)
+
+@app.route('/cancelflight')
+def cancelflight():
+    f_id = request.args.get('f_id')
+    with open('cancelflight.txt', 'r') as f:
+        query = f.read().replace('{f_id}', f_id)
+    cur = mysql.connection.cursor()
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
+    return str(data)
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=8000, debug=True)
