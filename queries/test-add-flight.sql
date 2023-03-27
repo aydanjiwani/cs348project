d36bdfd1ff13b205c440a8b274654c5a896e6e0a
John Li <johnnyli5555@gmail.com>
2023-03-27 16:53:21 -0400

    Update test-add-flight.sql

--- queries/test-add-flight.sql
+++ queries/test-add-flight.sql
@@ -1,5 +1,2 @@
-CREATE PROCEDURE IF NOT EXISTS flight(IN route_id INT, IN airline_id INT, IN airplane_id INT, IN deperature_time DATETIME, IN end_time DATETIME, IN st VARCHAR(20))
-BEGIN
-	INSERT INTO flights (ID, route_id, airline_id, airplane_id, deperature_time, end_time, st)
-        VALUES (ID, route_id, airline_id, airplane_id, deperature_time, end_time, st);
-END
+INSERT INTO flights (ID, route_id, airline_id, airplane_id, deperature_time, end_time, st)
+VALUES ({ID}, {route_id}, {airline_id}, {airplane_id}, {deperature_time}, {end_time}, {st});
