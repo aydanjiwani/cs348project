559fde46de544f0d0027fc9563403c98697d6aea
Noredeen Al-Zu'bi <noredeenalzubi@gmail.com>
2023-03-27 18:58:30 -0400

    add route_id to flight seeding (this makes it very slow oops rip CPU)

--- data/seed_flights.py
+++ data/seed_flights.py
@@ -18,8 +18,8 @@ with engine.connect() as connection:
         for chunk in flights:
 
             print('processing chunk...')
-            sql = f'''INSERT INTO Flights(flight_number,airline_code,departure_time,duration_minutes,distance_miles) VALUES
-                {",".join([f'("{row["flight_number"]}","{row["airline_code"]}","{row["departure_time"]}",{row["duration_minutes"]},{row["distance_miles"]})' for i, row in chunk.iterrows()])};
+            sql = f'''INSERT INTO Flights(route_id, flight_number,airline_code,departure_time,duration_minutes,distance_miles) VALUES
+                {",".join([f'((SELECT ID FROM Routes WHERE origin_ap_code="{row["origin_ap_code"]}" AND dest_ap_code="{row["dest_ap_code"]}"),"{row["flight_number"]}","{row["airline_code"]}","{row["departure_time"]}",{row["duration_minutes"]},{row["distance_miles"]})' for i, row in chunk.iterrows()])};
             '''
             connection.execute(text(sql))
 
@@ -38,3 +38,9 @@ with engine.connect() as connection:
     print('processing clean_flights5...')
     seed_flights_dataset('clean_flights5.csv')
     connection.commit()
+
+    update = '''UPDATE Flights
+                INNER JOIN Routes ON Routes.origin_ap_code = Flights.origin
+
+                )
+             '''
