7b7b502a1cec509909a10e03a7ba1253ff72616f
John Li <Johnnyli5555@.com>
2023-04-02 00:53:25 -0400

    added email function

--- queries/get-passengers.sql
+++ queries/get-passengers.sql
@@ -1,7 +1,8 @@
-SELECT f.departure_time,
-    r.origin_ap_code,
-    p.name,
-    p.email
+SELECT f.departure_time as departure_time,
+    r.origin_ap_code as origin,
+    r.dest_ap_code as destination,
+    p.name as name,
+    p.email as email
 FROM Flights as f
     LEFT JOIN Routes as r ON (f.route_id = r.ID)
     LEFT JOIN Ticket as t ON (t.flight_id = f.ID)
